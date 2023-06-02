00ec44ff50eaa5a0decde86aace51992431d471c
Pchek0 <92458106+PachecoASIR@users.noreply.github.com>
2023-06-02 10:36:17 +0200

    Actualización Ficheros
    Updateamos ficheros para solicitar nuevos datos al usuario o darle nuevas acceso a nuevas funcionalidades. También se corrigen pequeños errores como los paquetes en los que se encuentran los ficheros.

---
+++ Vista/Controlador/controlador.java
@@ -0,0 +1,77 @@
+package Controlador;
+
+import Vista.CatalogoEstilo;
+import Modelo.metodos;
+import Modelo.usuario;
+import Vista.Login;
+import Vista.Registro;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+
+public class controlador implements ActionListener{
+    
+    metodos met= new metodos();
+    usuario x = new usuario();
+    Login log = new Login();
+    Registro reg = new Registro();
+    CatalogoEstilo cata = new CatalogoEstilo();       
+    
+    public controlador(Login log,Registro reg,usuario user){
+ this.log = log; 
+    this.reg=reg;
+ 
+    this.x=user;
+    
+    ///registro de usuarios
+    this.reg.btnRegistrarse.addActionListener(this);
+    ///login de usuario
+     this.log.btnLogin.addActionListener(this);
+    
+    }
+    
+   public void Registrar(){
+   x.setNombre(reg.txtNombre.getText());
+   x.setApellidos(reg.txtApellidos.getText());
+   x.setContraseña(reg.txtPass.getText());
+   x.setCorreo(reg.txtCorreo.getText());
+   
+   if(met.InsertarUsuario(x)){
+   reg.setVisible(false);
+   log.setVisible(true);
+       JOptionPane.showMessageDialog(null,"Usuario registrado correctamente");
+   
+   }else{
+   JOptionPane.showMessageDialog(null,"Error al regisitrarse");
+   }
+   
+   }
+    
+   public void Ingreso(){
+   String us=log.logNombre.getText();
+   String pw=log.logContraseña.getText();
+   
+   if(met.Autentificacion(us,pw)){
+       log.setVisible(false);
+   cata.setVisible(true);
+    JOptionPane.showMessageDialog(null,"Usuario logueado");
+   }else{
+    JOptionPane.showMessageDialog(null,"Usuario no registrado");
+   }
+   }
+   
+
+    @Override    
+    public void actionPerformed(ActionEvent e) {
+        
+        if(e.getSource() == reg.btnRegistrarse){
+        Registrar();
+        }
+        if(e.getSource()== log.btnLogin){
+        Ingreso();
+        }
+    }
+    
+    
+}
---
+++ Vista/Modelo/metodos.java
@@ -0,0 +1,57 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class metodos {
+
+    public static boolean InsertarUsuario(usuario x) {
+
+        Connection cn = Conexion.Conectar();
+        PreparedStatement ps = null;
+        String sql = "insert into usuario (Nombre,Apellidos,Contraseña,Correo) values (?,?,?,?)";
+
+        try {
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, x.getNombre());
+            ps.setString(2, x.getApellidos());
+            ps.setString(3, x.getContraseña());
+            ps.setString(4, x.getCorreo());
+            ps.execute();
+            ps.close();
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println(e);
+
+        }
+        return false;
+    }
+    
+    public static boolean Autentificacion(String Puser,String Ppsw){
+        Connection cn = Conexion.Conectar();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String sql="select Nombre,Contraseña from usuario where Nombre=? and Contraseña=?";
+        try{
+        ps=cn.prepareStatement(sql);
+        ps.setString(1, Puser);
+        ps.setString(2, Ppsw);
+        rs=ps.executeQuery();
+        
+        while(rs.next()){
+        cn.close();
+        return true;
+        }
+        
+        }catch (Exception e){
+        System.out.println(e);
+        
+        }
+    return false;
+    }
+    
+}
